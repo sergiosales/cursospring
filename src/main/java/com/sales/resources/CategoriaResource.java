05ec3925f2a5b167a92184573a317229976a4c90
sergiosales <paulounopar@hotmail.com>
2021-07-17 17:58:41 -0300

    testando o rest

---
+++ src/main/java/com/sales/resources/CategoriaResource.java
@@ -0,0 +1,21 @@
+package com.sales.resources;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)	
+	public String listar() {
+		
+		return "O Rest esta funcionando";
+				
+		
+	}
+
+}
