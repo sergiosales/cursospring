1ba8eb59ebb928d9789ff335ed8f34e27849182b
sergiosales <paulounopar@hotmail.com>
2021-07-17 19:14:00 -0300

    Listar a classe de dominio - Categoria

---
+++ src/main/java/com/sales/domain/Categoria.java
@@ -0,0 +1,71 @@
+package com.sales.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer codigo;
+	private String nome;
+	
+	public Categoria() {}
+	
+	
+
+	public Categoria(Integer codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+
+}
--- src/main/java/com/sales/resources/CategoriaResource.java
+++ src/main/java/com/sales/resources/CategoriaResource.java
@@ -1,9 +1,14 @@
 package com.sales.resources;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sales.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -11,9 +16,15 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)	
-	public String listar() {
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(1, "Escritorio");
 		
-		return "O Rest esta funcionando";
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 				
 		
 	}
