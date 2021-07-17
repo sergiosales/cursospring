66dbfd66a4e1c271622836f84b6e176b78ffca10
sergiosales <paulounopar@hotmail.com>
2021-07-17 17:38:48 -0300

    projeto criado

---
+++ src/main/java/com/sales/ProjetoSpringsApplication.java
@@ -0,0 +1,13 @@
+package com.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringsApplication.class, args);
+	}
+
+}
