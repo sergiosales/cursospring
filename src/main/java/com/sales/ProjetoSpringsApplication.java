e8c82444beae78ff0135d0282808953095e47e0f
sergiosales <paulounopar@hotmail.com>
2021-07-19 12:06:50 -0300

    criando operação de instaciação

--- src/main/java/com/sales/ProjetoSpringsApplication.java
+++ src/main/java/com/sales/ProjetoSpringsApplication.java
@@ -1,13 +1,32 @@
 package com.sales;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sales.domain.Categoria;
+import com.sales.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoSpringsApplication {
+public class ProjetoSpringsApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	 Categoria cat  = new Categoria(null, "Informatica");
+	 Categoria cat1  = new Categoria(null, "Escritorio");
+	 
+	 categoriaRepository.saveAll(Arrays.asList(cat,cat1));
+		
 	}
 
 }
