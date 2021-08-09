47b149f15ab275502a044372e7792d17ba94edc9
sergiosales <paulounopar@hotmail.com>
2021-08-09 20:16:17 -0300

    Personalizando o retorno da Validação

---
+++ src/main/java/com/sales/resources/excecption/ValidationError.java
@@ -0,0 +1,27 @@
+package com.sales.resources.excecption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<FieldMessage>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String messagem){
+		errors.add(new FieldMessage(fieldName, messagem));
+	}
+	
+	
+
+}
