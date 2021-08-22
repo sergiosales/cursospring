bfd8f1b6c9759c85b59f8ceb44372e3519c7b1a9
sergiosales <paulounopar@hotmail.com>
2021-08-22 14:21:06 -0300

    Validação de email para não houver repetição na inserção

--- src/main/java/com/sales/repository/ClienteRepository.java
+++ src/main/java/com/sales/repository/ClienteRepository.java
@@ -4,10 +4,13 @@ package com.sales.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sales.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/sales/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/sales/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sales.domain.Cliente;
 import com.sales.domain.dto.ClienteNewDTO;
 import com.sales.domain.enuns.TipoCliente;
+import com.sales.repository.ClienteRepository;
 import com.sales.resources.excecption.FieldMessage;
 import com.sales.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository rep;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PessoaJuridica.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage ("CpfOuCnpj","Cnpj Inválido"));
 			
+		}
+		
+		Cliente auxiliar = rep.findByEmail(objDto.getEmail());
+		
+		if(auxiliar!= null) {
+			
+			list.add(new FieldMessage("Email","Email ja existente"));
 		}
 		
 		for (FieldMessage e : list) {
