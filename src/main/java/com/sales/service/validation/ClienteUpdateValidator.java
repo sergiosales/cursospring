21fcefbf8a1aab90b2c03a603df9d6e6536e8cd3
sergiosales <paulounopar@hotmail.com>
2021-08-22 14:53:41 -0300

    validação de email na atualização do cliente

--- src/main/java/com/sales/service/validation/ClienteUpdateValidator.java
+++ src/main/java/com/sales/service/validation/ClienteUpdateValidator.java
@@ -2,44 +2,44 @@ package com.sales.service.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.sales.domain.Cliente;
-import com.sales.domain.dto.ClienteNewDTO;
-import com.sales.domain.enuns.TipoCliente;
+import com.sales.domain.dto.ClienteDTO;
 import com.sales.repository.ClienteRepository;
 import com.sales.resources.excecption.FieldMessage;
-import com.sales.service.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private ClienteRepository rep;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 // inclua os testes aqui, inserindo erros na lista
-		if(objDto.getTipo().equals(TipoCliente.PessoaFisica.getCod()) && !BR.isValidCpf(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage ("CpfOuCnpj","Cpf Inválido"));
-			
-		}
-		if(objDto.getTipo().equals(TipoCliente.PessoaJuridica.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage ("CpfOuCnpj","Cnpj Inválido"));
-			
-		}
+		
+	
+		@SuppressWarnings({ "unchecked", "unused" })
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriCodigo  = Long.parseLong(map.get("codigo"));
 		
 		Cliente auxiliar = rep.findByEmail(objDto.getEmail());
 		
-		if(auxiliar != null) {
+		if(auxiliar != null && !auxiliar.getCodigo().equals(uriCodigo)) {
 			
 			list.add(new FieldMessage("Email","Email ja existente"));
 		}
